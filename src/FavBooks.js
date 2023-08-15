b6ac8b19603856daaac5d0264cde86e105200520
unknown <nickschaper@hotmail.com>
2023-08-15 08:47:12 -0400

    Init

---
+++ src/FavBooks.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class FavBooks extends Component {
+    render() {
+        return (
+            <div>
+                <h2>JPG Pic</h2>
+                <p>Here is a JPG Pic of MetLife Stadium
+                </p>
+                <img src="/metlife.jpg" alt="My Image" />
+            </div>
+        );
+    }
+}
+
+export default FavBooks;
